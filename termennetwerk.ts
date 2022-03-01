ea0ce8771299b7d118e4f07636457b10769ad365
Enno <ennomeijers@protonmail.com>
2022-03-01 14:30:14 +0100

    Fix: NoT matching
    Trap errormsg from NoT and improve matching.

--- termennetwerk.ts
+++ termennetwerk.ts
@@ -13,6 +13,7 @@ interface GraphQlResponse {
         prefLabel: string[];
         altLabel: string[];
       }[];
+      message: string;
     };
   }[];
 }
@@ -51,22 +52,33 @@ export async function getUriOfSearchTerm(sources: string[], searchTerm: string):
     const cacheKey = searchTerm.toLowerCase();
     if (cache[cacheKey]) return cache[cacheKey];
     const response: GraphQlResponse = await client.request(graphQlQuery, { sources, query: searchTerm });
-    if (!Array.isArray(response.terms) ||
-        response.terms[0].result.terms.length !== 1 ||
-        !response.terms[0].result ||
-        !response.terms[0].result.terms ||
-        !Array.isArray(response.terms[0].result.terms)) {
+    if (!Array.isArray(response.terms[0].result.terms) ||   // terms[] does not exists when an error occurs like a timeout
+        response.terms[0].result.terms.length !== 1 ) {     // allow only a single result
+          if (response.terms[0].result.message) {
+            console.error('Error message from Network-of-Terms API: ',response.terms[0].result.message,' for keyword: ', searchTerm);
+          }
           return undefined;
     }
-
     /*
       searchTerm 'fietsen (transportmiddelen)' => http://vocab.getty.edu/aat/300212636 (prefLabel)
       searchTerm 'rijwiel' => http://vocab.getty.edu/aat/300212636 (altLabel)
       searchTerm 'rijwielen' => http://vocab.getty.edu/aat/300212636 (altLabel)
       searchTerm 'fiets' => undefined (no match with prefLabel or altLabel)
     */
-    const term = response.terms[0].result.terms.find((term) => term.prefLabel.includes(searchTerm) || term.altLabel.includes(searchTerm));
-    if (term === undefined) return undefined;
+    const term = response.terms[0].result.terms[0];
+    const inPrefLabel = term.prefLabel.find(
+      (label) => {
+         return label.toLowerCase() === searchTerm.toLowerCase();
+      } 
+    );
+
+    const inAltLabel = term.altLabel.find(
+      (label) => {
+        return label.toLowerCase() === searchTerm.toLowerCase();
+      } 
+    );
+    if (!inPrefLabel && !inAltLabel ) return undefined;
+    
     cache[cacheKey] = term.uri;
     return term.uri;
 }
