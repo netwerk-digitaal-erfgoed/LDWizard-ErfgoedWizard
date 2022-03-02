85973e9cb0351c6b3820161b0192a9e91ebdf700
Enno <ennomeijers@protonmail.com>
2022-03-02 13:28:53 +0100

    Fix: NoT matching improvements

--- termennetwerk.ts
+++ termennetwerk.ts
@@ -22,6 +22,7 @@ const graphQlQuery = gql`
     terms(
       sources: $sources
       query: $query
+      queryMode: OPTIMIZED
     ) {
       source {
         name
@@ -52,10 +53,11 @@ export async function getUriOfSearchTerm(sources: string[], searchTerm: string):
     const cacheKey = searchTerm.toLowerCase();
     if (cache[cacheKey]) return cache[cacheKey];
     const response: GraphQlResponse = await client.request(graphQlQuery, { sources, query: searchTerm });
-    if (!Array.isArray(response.terms[0].result.terms) ||   // terms[] does not exists when an error occurs like a timeout
-        response.terms[0].result.terms.length !== 1 ) {     // allow only a single result
-          if (response.terms[0].result.message) {
-            console.error('Error message from Network-of-Terms API: ',response.terms[0].result.message,' for keyword: ', searchTerm);
+    const result = response.terms[0].result;
+    if (!Array.isArray(result.terms) ||   // terms[] does not exists when an error occurs like a timeout
+        result.terms.length !== 1 ) {     // allow only a single result
+          if (result.message) {
+            console.error(`Error message from Network-of-Terms API: "${result.message}" for keyword: "${searchTerm}"`);
           }
           return undefined;
     }
@@ -66,17 +68,10 @@ export async function getUriOfSearchTerm(sources: string[], searchTerm: string):
       searchTerm 'fiets' => undefined (no match with prefLabel or altLabel)
     */
     const term = response.terms[0].result.terms[0];
-    const inPrefLabel = term.prefLabel.find(
-      (label) => {
-         return label.toLowerCase() === searchTerm.toLowerCase();
-      } 
-    );
 
-    const inAltLabel = term.altLabel.find(
-      (label) => {
-        return label.toLowerCase() === searchTerm.toLowerCase();
-      } 
-    );
+    const containsSearchTerm = (label: string) => label.toLowerCase() === searchTerm.toLowerCase();
+    const inPrefLabel = term.prefLabel.some(containsSearchTerm);
+    const inAltLabel = term.altLabel.some(containsSearchTerm);
     if (!inPrefLabel && !inAltLabel ) return undefined;
     
     cache[cacheKey] = term.uri;
