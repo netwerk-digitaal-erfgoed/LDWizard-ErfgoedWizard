955f351a633d642c3dfba5c112913164161f7c7a
Enno <h.j.meijers@gmail.com>
2023-11-27 19:47:59 +0100

    Updated to latest core and TN rewrite

--- config.ts
+++ config.ts
@@ -1,8 +1,5 @@
-import WizardConfig, { PrefixEntry } from "@pldn/ldwizard/types/WizardConfig";
-import TermennetwerkTransformationAAT from "./termennetwerkTransformationAAT";
-import TermennetwerkTransformationCHT from "./termennetwerkTransformationCHT";
-import TermennetwerkTransformationWDPersons from "./termennetwerkTransformationWDPersons";
-import TermennetwerkTransformationWDPlaces from "./termennetwerkTransformationWDPlaces";
+import WizardConfig, { ColumnRefinement, PrefixEntry } from "@pldn/ldwizard/types/WizardConfig";
+import { getUriOfSearchTerm, getRefinementList } from "./termennetwerk";
 
 // @ts-ignore
 import img from "./ndelogo.png";
@@ -13,6 +10,42 @@ import favIcon from "./favicon.png";
 // @ts-ignore
 import homePage from "./homePage.md";
 
+var sourceList=await getRefinementList();
+
+const refinements:ColumnRefinement[] = [
+  {
+    label: "Verwerk als URI/IRI",
+    type: "single",
+    description:
+    "In this transformation the returned value should be an IRI, this can be applied to the 'IRIs' column in the example.csv file",
+    transformation: async (term: string) => {
+      return `${term}`;
+    },
+    yieldsIri: true,
+    keepOriginalValue: {
+      keepValue: false,
+    },
+  },
+/*
+  {
+    label: "Termennetwerk - Concepten",
+    type:"single",
+    description:
+      "Deze transformatie gebruikt het Termennetwerk om waardes in deze kolom te vervangen door de AAT-URIs van deze termen mits exact gematched. Zie de 'enriched csv' voor de details van deze matching. Voer de definitieve conversie hierop uit na controle van de matches.",
+    transformation: async (searchTerm: string) => {
+      const sources = [
+        "http://vocab.getty.edu/aat/sparql",
+        "https://data.cultureelerfgoed.nl/PoolParty/sparql/term/id/cht",
+      ];
+      return getUriOfSearchTerm(sources, searchTerm);
+    },
+    yieldsIri: true
+  },
+*/
+];
+
+const list = refinements.concat(sourceList);
+
 const wizardConfig: WizardConfig = {
   appName: "LDWizard - Erfgoed",
   defaultBaseIri: "https://data.netwerkdigitaalerfgoed.nl/",
@@ -22,6 +55,8 @@ const wizardConfig: WizardConfig = {
   publishOrder: ["download"],
   icon: img,
   favIcon: favIcon,
+  classConfig: { method:"elastic", endpoint: "https://api.data.netwerkdigitaalerfgoed.nl/datasets/NDE/ldwizard/services/ldwizard/_search" },
+  predicateConfig: { method:"elastic", endpoint: "https://api.data.netwerkdigitaalerfgoed.nl/datasets/NDE/ldwizard/services/ldwizard/_search" },
   dataplatformLink: "https://data.netwerkdigitaalerfgoed.nl/",
   repositoryLink: "https://github.com/netwerk-digitaal-erfgoed/LDWizard-ErfgoedWizard",
   getAllowedPrefixes: async () => {
@@ -33,11 +68,6 @@ const wizardConfig: WizardConfig = {
     console.error("Prefixes retrieval failed")
     return [];
   },
-  columnRefinements: [
-    TermennetwerkTransformationAAT,
-    TermennetwerkTransformationCHT,
-    TermennetwerkTransformationWDPersons,
-    TermennetwerkTransformationWDPlaces,
-  ],
-};
-globalThis.wizardConfig = wizardConfig;
+  columnRefinements: list
+} 
+export default globalThis.wizardConfig = wizardConfig;
--- termennetwerk.ts
+++ termennetwerk.ts
@@ -1,24 +1,38 @@
 import { GraphQLClient, gql } from "graphql-request";
+import { ColumnRefinement } from "@pldn/ldwizard/types/WizardConfig";
 
 const endpoint = "https://termennetwerk-api.netwerkdigitaalerfgoed.nl/graphql";
 
-interface GraphQlResponse {
+interface Term {
+  uri: string;
+  prefLabel: string[];
+  altLabel: string[];
+}
+
+interface Result {
+  __typename: string;
+  terms: Term[];
+  message: string;
+};
+
+interface TermsResponse {
   terms: {
     source: {
       name: string;
     };
-    result: {
-      __typename: string;
-      terms: {
-        uri: string;
-        prefLabel: string[];
-        altLabel: string[];
-      }[];
-      message: string;
-    };
+    result: Result;
   }[];
 }
-const graphQlQuery = gql`
+
+interface SourcesResponse {
+  sources: {
+    uri: string,
+    name: string,
+    description: string
+  }[];
+}
+
+const queryTerms = gql`
   query Terms($sources: [ID]! $query: String!) {
     terms(
       sources: $sources
@@ -45,29 +59,93 @@ const graphQlQuery = gql`
   }
 `;
 
+const querySources = gql`
+  query Sources {
+    sources {
+      uri
+      name
+    }
+  }
+`;
+
 const cache: { [query: string]: string } = {};
 
 const client = new GraphQLClient(endpoint);
 
-export async function getUriOfSearchTerm(sources: string[], searchTerm: string): Promise<string | undefined> {
-    if (searchTerm === "") return undefined;
-    const cacheKey = searchTerm.toLowerCase();
-    if (cache[cacheKey]) return cache[cacheKey];
-    const response: GraphQlResponse = await client.request(graphQlQuery, { sources, query: searchTerm });
-    const result = response.terms[0].result;
-//    if (!Array.isArray(result.terms) ||   
-    if (result.__typename === 'TimeoutError') { // catch timeouts errors
-      console.error(`Timeout error from Network-of-Terms API: "${result.message}" for keyword: "${searchTerm}"`);
+function selectTerm(result: Result, searchTerm: string, source: string): Term | undefined {
+
+    if (result.__typename === 'ServerError') { // catch server errors
+      //console.log("Termennetwerk refinement: server error for source",source);
       return undefined; 
     }
-    if (result.terms.length !== 1 ) return undefined; // allow only a single result
-          
-    const term = response.terms[0].result.terms[0];
-    const containsSearchTerm = (label: string) => label.toLowerCase() === searchTerm.toLowerCase();
-    const inPrefLabel = term.prefLabel.some(containsSearchTerm); // exact match for prefLabel?
-    const inAltLabel = term.altLabel.some(containsSearchTerm);   // exact match for altLabel?
-    if (!inPrefLabel && !inAltLabel ) return undefined; // no matches found
-    
-    cache[cacheKey] = term.uri;
-    return term.uri;
+
+    if (result.__typename === 'TimeoutError') { // catch timeouts errors
+      //console.log("Termennetwerk refinement: time out for source", source);
+      return undefined; 
+    }
+
+    if (result.terms.length === 0 ) { // no results found
+      //console.log("Termennetwerk refinement: no results for",searchTerm,"in",source);
+      return undefined; 
+    }
+
+    var countMatches = 0;
+    var matchedTerm:Term = <Term>{};
+    for( const nr in result.terms ) {
+      const term = result.terms[nr];
+      const containsSearchTerm = (label: string) => label.toLowerCase() === searchTerm.toLowerCase();
+      const inPrefLabel = term.prefLabel.some(containsSearchTerm); // exact match for prefLabel?
+      const inAltLabel = term.altLabel.some(containsSearchTerm);   // exact match for altLabel?
+      if ( inPrefLabel || inAltLabel ) {
+        countMatches=+1;
+        matchedTerm=term;
+      }
+    }
+    if(countMatches==1){
+      //console.log("Termennetwerk refinement: found single exact match for",searchTerm,"in",source);
+      return matchedTerm;
+    }
+    else {
+      //console.log("Termennetwerk refinement: no single exact match found for",searchTerm,"in",source);
+      return undefined; // no matches found
+    }
+}
+
+export async function getUriOfSearchTerm(sources: string[], searchTerm: string): Promise<string | undefined> {
+    if (searchTerm.trim() === "") return undefined;
+    const cacheKey = searchTerm.toLowerCase();
+    if (cache[cacheKey]) return cache[cacheKey];
+    const response: TermsResponse = await client.request(queryTerms, { sources, query: searchTerm });
+    for( const term in response){
+        for( const sourcenr in response[term]) {
+          const source = response[term][sourcenr].source.name;
+          const result = response[term][sourcenr].result;
+          const selectedTerm = selectTerm(result, searchTerm, source);
+          if( selectedTerm ) {
+            cache[cacheKey] = selectedTerm.uri;
+            return selectedTerm.uri;
+          }
+        }
+    }
+    return undefined;
+}
+
+export async function getRefinementList():Promise<ColumnRefinement[]> {
+  const response: SourcesResponse = await client.request(querySources);
+  var refinementList: ColumnRefinement[] = [];
+  for( const sourcenr in response.sources ){
+    //console.log("Source loop index",sourcenr);
+    const refinement:ColumnRefinement = {
+      label: "Termennetwerk: "+response.sources[sourcenr].name,
+      type: "single",
+      description: response.sources[sourcenr].description,
+      transformation: async (searchTerm: string) => {
+        const sources = [response.sources[sourcenr].uri];
+        return getUriOfSearchTerm(sources, searchTerm);
+      },
+      yieldsIri: true
+    }
+    refinementList.push(refinement)
+  }
+  return refinementList;
 }
