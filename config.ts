09528610476444356d737b44a4dd4a6847e2291d
Gerwin Bosch <gerwin.bosch@triply.cc>
2024-03-26 13:43:15 +0100

    feat: Update to LDWizard v4 (#57)
    * Feat: Update to LDWizard v4
    
    * Update to v4.0.1

--- config.ts
+++ config.ts
@@ -10,41 +10,11 @@ import favIcon from "./favicon.png";
 // @ts-ignore
 import homePage from "./homePage.md";
 
-var sourceList=await getRefinementList();
+async function initSources() {
+  var sourceList = await getRefinementList();
 
-const refinements:ColumnRefinement[] = [
-  {
-    label: "Verwerk als URI/IRI",
-    type: "single",
-    description:
-    "In this transformation the returned value should be an IRI, this can be applied to the 'IRIs' column in the example.csv file",
-    transformation: async (term: string) => {
-      return `${term}`;
-    },
-    yieldsIri: true,
-    keepOriginalValue: {
-      keepValue: false,
-    },
-  },
-/*
-  {
-    label: "Termennetwerk - Concepten",
-    type:"single",
-    description:
-      "Deze transformatie gebruikt het Termennetwerk om waardes in deze kolom te vervangen door de AAT-URIs van deze termen mits exact gematched. Zie de 'enriched csv' voor de details van deze matching. Voer de definitieve conversie hierop uit na controle van de matches.",
-    transformation: async (searchTerm: string) => {
-      const sources = [
-        "http://vocab.getty.edu/aat/sparql",
-        "https://data.cultureelerfgoed.nl/PoolParty/sparql/term/id/cht",
-      ];
-      return getUriOfSearchTerm(sources, searchTerm);
-    },
-    yieldsIri: true
-  },
-*/
-];
-
-const list = refinements.concat(sourceList);
+  globalThis.wizardConfig.columnRefinements.push(...sourceList);
+}
 
 const wizardConfig: WizardConfig = {
   appName: "LDWizard - Erfgoed",
@@ -55,8 +25,14 @@ const wizardConfig: WizardConfig = {
   publishOrder: ["download"],
   icon: img,
   favIcon: favIcon,
-  classConfig: { method:"elastic", endpoint: "https://api.data.netwerkdigitaalerfgoed.nl/datasets/NDE/ldwizard/services/ldwizard/_search" },
-  predicateConfig: { method:"elastic", endpoint: "https://api.data.netwerkdigitaalerfgoed.nl/datasets/NDE/ldwizard/services/ldwizard/_search" },
+  classConfig: {
+    method: "elastic",
+    endpoint: "https://api.data.netwerkdigitaalerfgoed.nl/datasets/NDE/ldwizard/services/ldwizard/_search",
+  },
+  predicateConfig: {
+    method: "elastic",
+    endpoint: "https://api.data.netwerkdigitaalerfgoed.nl/datasets/NDE/ldwizard/services/ldwizard/_search",
+  },
   dataplatformLink: "https://data.netwerkdigitaalerfgoed.nl/",
   repositoryLink: "https://github.com/netwerk-digitaal-erfgoed/LDWizard-ErfgoedWizard",
   getAllowedPrefixes: async () => {
@@ -65,9 +41,26 @@ const wizardConfig: WizardConfig = {
       const prefixes: PrefixEntry[] = await response.json();
       return prefixes;
     }
-    console.error("Prefixes retrieval failed")
+    console.error("Prefixes retrieval failed");
     return [];
   },
-  columnRefinements: list
-} 
+  columnRefinements: [
+    {
+      label: "Verwerk als URI/IRI",
+      type: "single",
+      description:
+        "In this transformation the returned value should be an IRI, this can be applied to the 'IRIs' column in the example.csv file",
+      transformation: async (term: string) => {
+        return `${term}`;
+      },
+      yieldsIri: true,
+      keepOriginalValue: {
+        keepValue: false,
+        owlSameAsRelationship: false,
+      },
+    },
+  ],
+};
+
 export default globalThis.wizardConfig = wizardConfig;
+initSources();
